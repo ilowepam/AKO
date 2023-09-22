9d7d13f70845bd6a0d4ba98c8158db5af39086ea
ilowepam <ilowepam@protonmail.com>
2023-09-22 16:13:26 +0200

    ONGOING DEVELOPMENT: setting up frontend with template App.vue / main.js

---
+++ frontend/ch/teko/ako/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+    <p>Counter: {{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Simple Counter App',
+      counter: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
